64b9c0a43c19194d2489d335489071e004e0e66a
sillyY <965768042@qq.com>
2021-03-16 13:54:19 +0800

    comments: add createCompilerCreator comment

--- src/compiler/create-compiler.js
+++ src/compiler/create-compiler.js
@@ -10,6 +10,8 @@ export function createCompilerCreator (baseCompile: Function): Function {
       template: string,
       options?: CompilerOptions
     ): CompiledResult {
+
+      // 通过Object.create以baseOptions为原型创建finalOptions
       const finalOptions = Object.create(baseOptions)
       const errors = []
       const tips = []
@@ -17,10 +19,15 @@ export function createCompilerCreator (baseCompile: Function): Function {
       let warn = (msg, range, tip) => {
         (tip ? tips : errors).push(msg)
       }
-
+      
+      // options存在
+      // |> $mount阶段调用compileToFunctions的第二个参数
       if (options) {
+
+        // 非生产环境且outputSourceRange设为true，
+        // |> 修改warn函数，补充错误开始位置start与结束位置end
         if (process.env.NODE_ENV !== 'production' && options.outputSourceRange) {
-          // $flow-disable-line
+          // 前置空白字符长度
           const leadingSpaceLength = template.match(/^\s*/)[0].length
 
           warn = (msg, range, tip) => {
@@ -36,19 +43,21 @@ export function createCompilerCreator (baseCompile: Function): Function {
             (tip ? tips : errors).push(data)
           }
         }
-        // merge custom modules
+        // 合并自定义modules
+        // baseOptions中的modules，然后可以附带自定义的modules，以实现拓展性
         if (options.modules) {
           finalOptions.modules =
             (baseOptions.modules || []).concat(options.modules)
         }
-        // merge custom directives
+        // 合并自定义directives
+        // baseOptions中的directives，然后可以附带自定义的directives，以实现拓展性
         if (options.directives) {
           finalOptions.directives = extend(
             Object.create(baseOptions.directives || null),
             options.directives
           )
         }
-        // copy other options
+        // 拷贝其他属性
         for (const key in options) {
           if (key !== 'modules' && key !== 'directives') {
             finalOptions[key] = options[key]
@@ -56,12 +65,20 @@ export function createCompilerCreator (baseCompile: Function): Function {
         }
       }
 
+      // finalOptions带上warn函数
       finalOptions.warn = warn
 
+      // baseCompile指的就是createCompiler中的parse、optimize和generate那个baseCompile函数
+      // |> 传入模板template和处理过的finalOptions
+      // |> compiled指的就是包含ast、render和staticRenderFns的对象
       const compiled = baseCompile(template.trim(), finalOptions)
+      
+      // 非正式环境校检抽象语法树ast中有问题的表达式
       if (process.env.NODE_ENV !== 'production') {
         detectErrors(compiled.ast, warn)
       }
+
+      // 给compiled对象添加errors和tips信息
       compiled.errors = errors
       compiled.tips = tips
       return compiled
