4a2c944e3933bb4d31d1eaa4961b4bae7ca626c4
sillyY <965768042@qq.com>
2021-03-16 13:09:57 +0800

    comments: add createCompilerCreator comment

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -5,17 +5,22 @@ import { optimize } from './optimizer'
 import { generate } from './codegen/index'
 import { createCompilerCreator } from './create-compiler'
 
-// `createCompilerCreator` allows creating compilers that use alternative
-// parser/optimizer/codegen, e.g the SSR optimizing compiler.
-// Here we just export a default compiler using the default parts.
+// createCompiler 允许创建使用 parser(解析器)、optimizer(优化器)、codegen(代码生成器)的编译器
+// 比如：SSR优化编译器，此处值使用默认编译器
 export const createCompiler = createCompilerCreator(function baseCompile (
   template: string,
   options: CompilerOptions
 ): CompiledResult {
+  
+  // 调用parse将template生成ast抽象语法树
   const ast = parse(template.trim(), options)
+
+  // 调用optimize，优化ast抽象语法树(标记静态节点)
   if (options.optimize !== false) {
     optimize(ast, options)
   }
+
+  // 将ast转成render和staticRenderFns
   const code = generate(ast, options)
   return {
     ast,
