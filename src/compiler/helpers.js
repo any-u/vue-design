475fce48f9480587707b42af866ebcba2a8fb0ff
sillyY <965768042@qq.com>
2021-03-18 17:06:15 +0800

    comments: add addHandler comment

--- src/compiler/helpers.js
+++ src/compiler/helpers.js
@@ -77,8 +77,8 @@ export function addHandler (
   dynamic?: boolean
 ) {
   modifiers = modifiers || emptyObject
-  // warn prevent and passive modifier
-  /* istanbul ignore if */
+  
+  // 非生产环境，同时使用prevent修饰符和passive修饰符时，打印⚠️信息
   if (
     process.env.NODE_ENV !== 'production' && warn &&
     modifiers.prevent && modifiers.passive
@@ -90,9 +90,8 @@ export function addHandler (
     )
   }
 
-  // normalize click.right and click.middle since they don't actually fire
-  // this is technically browser-specific, but at least for now browsers are
-  // the only target envs that have right/middle clicks.
+  // 规范化鼠标右键事件.right修饰符和鼠标中键修饰符.middle
+  // |> 实际上它们不会触发，浏览器目前是唯一具有右键和中键点击的环境
   if (modifiers.right) {
     if (dynamic) {
       name = `(${name})==='click'?'contextmenu':(${name})`
@@ -108,22 +107,31 @@ export function addHandler (
     }
   }
 
-  // check capture modifier
+  // 检查.capture修饰符
+  // name -> !name
   if (modifiers.capture) {
     delete modifiers.capture
     name = prependModifierMarker('!', name, dynamic)
   }
+
+  // 检查.once修饰符
+  // name -> ~name
   if (modifiers.once) {
     delete modifiers.once
     name = prependModifierMarker('~', name, dynamic)
   }
-  /* istanbul ignore if */
+
+  // 检查.passive修饰符
+  // name -> &name
   if (modifiers.passive) {
     delete modifiers.passive
     name = prependModifierMarker('&', name, dynamic)
   }
 
   let events
+
+  // 处理.native修饰符
+  // |> .natvie 监听元素原生事件
   if (modifiers.native) {
     delete modifiers.native
     events = el.nativeEvents || (el.nativeEvents = {})
@@ -131,21 +139,31 @@ export function addHandler (
     events = el.events || (el.events = {})
   }
 
+  // 新建新的处理函数
   const newHandler: any = rangeSetItem({ value: value.trim(), dynamic }, range)
   if (modifiers !== emptyObject) {
     newHandler.modifiers = modifiers
   }
 
+  // 从events事件中取出当前事件的处理函数handlers
   const handlers = events[name]
-  /* istanbul ignore if */
+  
+  // 如果handlers是数组，则用数组方式添加
   if (Array.isArray(handlers)) {
     important ? handlers.unshift(newHandler) : handlers.push(newHandler)
+  
+  // 如果handlers是对象，则将事件的处理函数改成数组形式
+  // 初始化阶段，第一个处理函数执行后，事件的处理函数会是一个对象
   } else if (handlers) {
     events[name] = important ? [newHandler, handlers] : [handlers, newHandler]
+
+  // 初始阶段
   } else {
     events[name] = newHandler
   }
 
+  // 将el.plain设为false
+  // |> plain --> 该节点所对应的虚拟节点将不包含任何 VNodeData
   el.plain = false
 }
 
