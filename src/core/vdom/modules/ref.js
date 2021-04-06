cd5e427889971b881b56ffa52f1ec82c8a18ac2f
sillyY <965768042@qq.com>
2021-04-06 10:23:39 +0800

    comments: registerRef comment

--- src/core/vdom/modules/ref.js
+++ src/core/vdom/modules/ref.js
@@ -17,13 +17,23 @@ export default {
   }
 }
 
+/**
+ * 注册ref
+ * |> 将vnode的组件实例(componentInstance) 或vnode的元素属性(elm)添加到vm.$refs中
+ */
 export function registerRef (vnode: VNodeWithData, isRemoval: ?boolean) {
+
+  // 用key保存ref的值
   const key = vnode.data.ref
   if (!isDef(key)) return
 
   const vm = vnode.context
   const ref = vnode.componentInstance || vnode.elm
   const refs = vm.$refs
+
+  // 如果是移除ref属性的情况
+  // |> refs[key]是数组，则遍历数组设置ref属性
+  // |> 如果refs[key]是ref，则直接设置undefined
   if (isRemoval) {
     if (Array.isArray(refs[key])) {
       remove(refs[key], ref)
@@ -31,6 +41,10 @@ export function registerRef (vnode: VNodeWithData, isRemoval: ?boolean) {
       refs[key] = undefined
     }
   } else {
+
+    // 判断是否在v-for下
+    // |> 不是 -> 直接设置到ref中
+    // |> 是  -> 数组，或非数组，2种情况做添加操作
     if (vnode.data.refInFor) {
       if (!Array.isArray(refs[key])) {
         refs[key] = [ref]
